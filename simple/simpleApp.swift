e5ff07eb57588956a24970df02cb1e1db4214420
Francescocrivelli <francesco.crivelli21@gmail.com>
2025-03-21 09:58:39 -0700

    Initial Commit

---
+++ simple/simpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleApp.swift
+//  simple
+//
+//  Created by Francesco Crivelli on 3/21/25.
+//
+
+import SwiftUI
+
+@main
+struct simpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
