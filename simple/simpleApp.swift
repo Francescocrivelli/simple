d43b5b7ae9da95f6783905c183ccf43670af9f19
Francesco Crivelli <46250577+Francescocrivelli@users.noreply.github.com>
2025-03-23 20:17:15 -0700

    Update App file

--- simple/simpleApp.swift
+++ simple/simpleApp.swift
@@ -1,17 +1,21 @@
-//
-//  simpleApp.swift
-//  simple
-//
-//  Created by Francesco Crivelli on 3/21/25.
-//
-
 import SwiftUI
 
 @main
-struct simpleApp: App {
+struct SimpleApp: App {
+    @StateObject private var authService = AuthService.shared
+    @StateObject private var subscriptionService = SubscriptionService.shared
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(authService)
+                .environmentObject(subscriptionService)
+                .onAppear {
+                    // Check subscription status and user session
+                    Task {
+                        await subscriptionService.checkSubscriptionStatus()
+                    }
+                }
         }
     }
 }
